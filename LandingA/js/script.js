dbd863b697cd8f7c99f8c8d1a7d4a0c45543bf67
vatraigor <vatra.igor.v@gmail.com>
2022-09-14 23:44:28 +0300

    Initial commit

--- LandingA/js/script.js
+++ LandingA/js/script.js
@@ -1,11 +1,21 @@
 $(document).ready(function () {
 //    $('.clients__slide-wrapper').slick({});
     $('.clients__slide-wrapper').slick({
-        //arrows: false,
+        arrows: false,
         dots: true,
         centerMode: true,
         centerPadding: '60px',
         slidesToShow: 3,
+        slidesToScroll: 1,
+        speed: 1000,
+        easing: 'ease',
+        infinite: false,
+        initialSlide: 0,
+        draggable: true,
+        swipe: true,
+        touchThreshold: 10,
+
+
         responsive: [
             {
                 breakpoint: 768,
