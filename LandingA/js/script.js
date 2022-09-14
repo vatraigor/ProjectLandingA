8e88fa16dbba2208807da36e5317c1617a5b012e
vatraigor <vatra.igor.v@gmail.com>
2022-09-14 23:44:28 +0300

    Initial commit

---
+++ LandingA/js/script.js
@@ -0,0 +1,30 @@
+$(document).ready(function () {
+//    $('.clients__slide-wrapper').slick({});
+    $('.clients__slide-wrapper').slick({
+        //arrows: false,
+        dots: true,
+        centerMode: true,
+        centerPadding: '60px',
+        slidesToShow: 3,
+        responsive: [
+            {
+                breakpoint: 768,
+                settings: {
+                    arrows: false,
+                    centerMode: true,
+                    centerPadding: '40px',
+                    slidesToShow: 3
+                }
+            },
+            {
+                breakpoint: 480,
+                settings: {
+                    arrows: false,
+                    centerMode: true,
+                    centerPadding: '40px',
+                    slidesToShow: 1
+                }
+            }
+        ]
+    });
+});
